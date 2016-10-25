f56010802ba31bbc398e301007ba756d4cc15391
Fredrik Boström <fredrik@fredrikbostrom.net>
2016-10-25 20:19:39 +0300

    Add clock component

--- app/js/app.js
+++ app/js/app.js
@@ -1,4 +1,19 @@
 angular.module('myapp', [])
-  .controller('MainCtrl', function () {
+
+  .controller('MainCtrl', function() {
     this.greeting = 'Hello Webpack!';
+  })
+
+  .component('currentTime', {
+    template: `
+      <div>{{$ctrl.time | date:'medium'}}</div>
+    `,
+    controller: function($timeout) {
+      var $ctrl = this;
+
+      (function tick() {
+        $ctrl.time = new Date();
+        $timeout(tick, 1000);
+      })();
+    }
   });
